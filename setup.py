0d522349c271dab3bb9c4d2d2f12f50dee05e404
Vincent Batts <vbatts@hashbangbash.com>
2011-08-19 19:43:10 -0400

    moving files around, for a more packagable layout

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import sys
+
+setup (name              = "pygtasks",
+       version           = "0.9.0",
+       description       = "pygtasks - A command line Python client for Google Tasks",
+       maintainer        = "Vincent Batts",
+       maintainer_email  = "vbatts@hashbangbash.com",
+       license           = "MIT",
+       long_description  = "pygtasks is a command line Python client for Google Tasks",
+       url               = "http://github.com/vbatts/pygtasks",
+       platforms         = ['Any'],
+       packages          = ['pygtasks','httplib2'],
+       py_modules        = ['gflags','gflags_validators'],
+       scripts           = ['bin/pygtasks.py','bin/translate_text.py'],
+       package_dir       = {'': 'lib/'},
+       )
+
